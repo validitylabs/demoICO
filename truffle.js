9b3eb7736606930a8e68865de387b1912436fdc7
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2017-09-13 09:48:13 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,10 +1,9 @@
 var Web3 = require("web3");
-let url = "https://micharoon.by.ether.camp:8555/sandbox/59768eb30c";
 
 module.exports = {
     networks: {
         development: {
-            provider: new Web3.providers.HttpProvider(url),
+            provider: new Web3.providers.HttpProvider(),
             network_id: "*"
         }
     }
